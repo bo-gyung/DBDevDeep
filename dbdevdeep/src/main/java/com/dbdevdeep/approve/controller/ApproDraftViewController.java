1205385b2a9c6162b9cbf58d58701fb0d03ea130
pbkan <pbkan4@gmail.com>
2024-09-26 19:06:28 +0900

    [박병준] 임시저장 완료

--- dbdevdeep/src/main/java/com/dbdevdeep/approve/controller/ApproDraftViewController.java
+++ dbdevdeep/src/main/java/com/dbdevdeep/approve/controller/ApproDraftViewController.java
@@ -11,16 +11,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.dbdevdeep.approve.domain.ApproDraftDto;
+import com.dbdevdeep.approve.domain.TempEditDto;
 import com.dbdevdeep.approve.service.ApproDraftService;
+import com.dbdevdeep.approve.service.TempEditService;
 
 @Controller
 public class ApproDraftViewController {
 
 	private final ApproDraftService approDraftService;
+	private final TempEditService tempEditService;
 	
 	@Autowired
-	public ApproDraftViewController(ApproDraftService approDraftService) {
+	public ApproDraftViewController(ApproDraftService approDraftService, TempEditService tempEditService) {
 		this.approDraftService = approDraftService;
+		this.tempEditService = tempEditService;
 	}
 	
 	// 보고서 임시함
@@ -39,6 +43,8 @@ public class ApproDraftViewController {
 		@GetMapping("/approvedraft/{draft_no}")
 		public String selectDraftOne(Model model , @PathVariable("draft_no") Long draft_no) {
 			ApproDraftDto dto = approDraftService.selectDraftOne(draft_no);
+			List<TempEditDto> tempList = tempEditService.callTemp();
+			model.addAttribute("tempList",tempList);
 			model.addAttribute("dto",dto);
 			return "approve/docuDraftDetail";
 		}
