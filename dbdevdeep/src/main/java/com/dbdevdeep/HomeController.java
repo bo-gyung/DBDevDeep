58c722d8e5f16a0a33650b8a8abb3859df36e379
jinsheo107 <jinsheo107@gmail.com>
2024-09-30 16:32:57 +0900

    [진서형] 충돌 해결

--- dbdevdeep/src/main/java/com/dbdevdeep/HomeController.java
+++ dbdevdeep/src/main/java/com/dbdevdeep/HomeController.java
@@ -19,7 +19,6 @@ import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
-//github.com/bo-gyung/DBDevDeep.git
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
